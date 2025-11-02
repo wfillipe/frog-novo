32246387f96e21ec32b4a475f9e00fb84626ee0e
wfillipe <wilson.fillipe@academico.ifpb.edu.br>
2025-11-02 16:31:57 -0300

    movimento

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float walkSpeed = 4f;
+    public float runSpeed = 7f;
+    public float jumpHeight = 2f;
+    public float gravity = -9.81f;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+
+
+        // Verifica se está no chão
+        isGrounded = controller.isGrounded;
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f; // mantém o player no chão
+
+        // Movimento horizontal
+        float moveZ = Input.GetAxis("Vertical");  // W/S ou 
+        float moveX = Input.GetAxis("Horizontal"); // A/D ou 
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+
+        // Escolhe velocidade normal ou corrida (Shift)
+        float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+
+        controller.Move(move * currentSpeed * Time.deltaTime);
+
+        // Pulo
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        // Aplicar gravidade
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
