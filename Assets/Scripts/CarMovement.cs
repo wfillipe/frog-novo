130c9ce4a149e2632183d9e723d4a56b18cc31ee
wfillipe <wilson.fillipe@academico.ifpb.edu.br>
2025-11-03 08:46:09 -0300

    script do carro quase certo

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -6,7 +6,7 @@ public class CarMovementSimple : MonoBehaviour
     public float speed = 5f;          // velocidade do carro
     public bool moveRight = true;     // define a direção: true = direita, false = esquerda
 
-    void Update()
+    void FixedUpdate()
     {
         float direction = moveRight ? 1f : -1f;
         transform.Translate(Vector3.right * direction * speed * Time.deltaTime);
---
+++ Assets/Scripts/CarStick.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CarStick : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.SetParent(transform);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        other.transform.SetParent(null);
+    }
+
+}
+
