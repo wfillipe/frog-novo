248bd01cc6f0766b8b0809980c1b7934b198e304
wfillipe <wilson.fillipe@academico.ifpb.edu.br>
2025-11-03 09:54:24 -0300

    melhorias

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -4,7 +4,7 @@ public class CarMovementSimple : MonoBehaviour
 {
     [Header("Configurações de movimento")]
     public float speed = 5f;          // velocidade do carro
-    public bool moveRight = true;     // define a direção: true = direita, false = esquerda
+    public bool moveRight = true;   // define a direção: true = frente (Z+), false = trás (Z-)
 
     void FixedUpdate()
     {
