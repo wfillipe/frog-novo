00b6fe6bdbfa48aab9b4ec50120b44c36026d876
wfillipe <wilson.fillipe@academico.ifpb.edu.br>
2025-11-03 09:22:02 -0300

    FUNCIONA

--- Assets/Scripts/CarStick.cs
+++ Assets/Scripts/CarStick.cs
@@ -1,30 +1,14 @@
 using UnityEngine;
 
-public class CarStickFixed : MonoBehaviour
+public class CarStick : MonoBehaviour
 {
-    private int objectsInside = 0; // conta quantos colliders do player estão dentro
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            objectsInside++;
-            other.transform.SetParent(transform);
-        }
+        other.transform.SetParent(transform);
     }
-
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            objectsInside--;
-
-            // só solta quando realmente saiu de todos os colliders
-            if (objectsInside <= 0)
-            {
-                other.transform.SetParent(null);
-                objectsInside = 0;
-            }
-        }
+        other.transform.SetParent(null);
     }
 }
